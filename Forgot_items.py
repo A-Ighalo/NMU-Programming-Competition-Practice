d2c009801a71eb9905a0fe60964ae4a6efd05c79
Abraham Ighalo <163622424+A-Ighalo@users.noreply.github.com>
2024-09-28 19:41:38 -0400

    -m Forgot_items

--- Forgot_items.py
+++ Forgot_items.py
@@ -1,5 +1,10 @@
-storeItems = []
-c = int(input().strip())
+# This script checks if you forgot any essential items (keys, phone, wallet) before heading out.
+# Input:
+#   The first line contains a single integer c (number of items grabbed).
+#   The next c lines each contain a single nonempty string of lowercase letters indicating an item grabbed.
+# Output:storeItems = []
+#   Each of "keys", "phone", and "wallet" that does not appear in the input, one per line in alphabetical order.c = int(input().strip())
+#   If none of these items are missing, print "ready".
 
 for i in range(c):
     storeItems.append(input())

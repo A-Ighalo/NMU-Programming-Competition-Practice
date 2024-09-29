f0647b879744ccc15a6d3260656d83d958a97cfb
Abraham Ighalo <163622424+A-Ighalo@users.noreply.github.com>
2024-09-28 21:34:01 -0400

    Update Forgot_items.py

--- Forgot_items.py
+++ Forgot_items.py
@@ -2,10 +2,11 @@
 # Input:
 #   The first line contains a single integer c (number of items grabbed).
 #   The next c lines each contain a single nonempty string of lowercase letters indicating an item grabbed.
-#   Each of "keys", "phone", and "wallet" that does not appear in the input, one per line in alphabetical order.c = int(input().strip())
+#   Each of "keys", "phone", and "wallet" that does not appear in the input, one per line in alphabetical order.
 #   If none of these items are missing, print "ready".
 
 storeItems = []
+c = int(input().strip())
 for i in range(c):
     storeItems.append(input())
 

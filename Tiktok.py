4c8ae7fd437d0f52b0c0a792bcc3aaba81aed1ed
Abraham Ighalo <163622424+A-Ighalo@users.noreply.github.com>
2024-10-13 21:29:36 -0400

    Reapply "Learning Madlibs"
    This reverts commit dfec98936c7ff8e4bbbd0730e63e7baa454daeec.

---
+++ Tiktok.py
@@ -0,0 +1,9 @@
+#remember to change the name to Halloween
+
+a = input().strip()
+
+if a == "OCT 31" or a == "OCT 31" or a == "DEC 25" or a == "DEC 25":
+    print("Yup")
+else:
+    print("nope")
+
